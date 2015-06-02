b9d71c2a30a4e3fa1a812dabf0722c08a838cc4d
Nitin <malhotra.nishu@gmail.com>
2015-06-02 22:20:39 +1000

    Read me doc updated

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,7 +8,7 @@ class JobsController < ApplicationController
       @category_id = Category.find_by(name: params[:category]).id
       @jobs = Job.where(category_id: @category_id).order("created_at DESC")
     end
-    
+
 	end
 	
 	def show
